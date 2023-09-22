68b18e27eef4a456ab12bc92eabff14196f17511
NelsonMALGAS <zmalgas69@gmail.com>
2023-09-22 08:06:30 +0200

    Modified files

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,10 +4,10 @@ import PostHeader from "./post-header";
 import Image from "next/image";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
-import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import javascript from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 
-SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("javascript", javascript);
 SyntaxHighlighter.registerLanguage("css", css);
 
 function PostContent(props) {
@@ -26,34 +26,33 @@ function PostContent(props) {
 		// 		/>
 		// 	);
 		// },
-		paragraph(paragraph) {
+		p(paragraph) {
 			const { node } = paragraph;
-		  console.log(node)
-			if (node.children[0] && node.children[0].type === "image") {
-			  const image = node.children[0];
-		  
-			  return (
-				<div className={classes.image}>
-				  <Image
-					src={`/images/posts/${post.slug}/${image.url}`}
-					alt={image.alt}
-					width={600}
-					height={300}
-				  />
-				</div>
-			  );
+			if (node.children[0] && node.children[0].tagName === "img") {
+				const image = node.children[0];
+
+				return (
+					<div className={classes.image}>
+						<Image
+							src={`/images/posts/${post.slug}/${image.properties.src}`}
+							alt={image.properties.alt}
+							width={600}
+							height={300}
+						/>
+					</div>
+				);
 			}
-		  
+
 			return <p>{paragraph.children}</p>;
-		  },
+		},
 
 		code(code) {
-			const { language, value } = code;
-
+			const { language = "", value } = code;
+			console.log(language);
 			return (
 				<SyntaxHighlighter
-					style={ atomDark }
-					language={language}
+					style={atomDark}
+					language={javascript}
 					children={value}
 				/>
 			);
@@ -63,7 +62,7 @@ function PostContent(props) {
 	return (
 		<article className={classes.content}>
 			<PostHeader title={post.title} image={imagePath} />
-			<ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+			<ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
 		</article>
 	);
 }
